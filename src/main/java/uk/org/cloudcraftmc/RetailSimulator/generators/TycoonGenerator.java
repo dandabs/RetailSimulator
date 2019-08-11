9e73ac8f629f237511d797a4adbfbe91050a72b6
Dan <dan@dans-mbp.home>
2019-08-11 16:33:36 +0100

    yes

--- src/main/java/uk/org/cloudcraftmc/RetailSimulator/generators/TycoonGenerator.java
+++ src/main/java/uk/org/cloudcraftmc/RetailSimulator/generators/TycoonGenerator.java
@@ -15,6 +15,21 @@ public class TycoonGenerator extends ChunkGenerator {
 
     int currentHeight = 50;
 
+    /*@Override
+    public ChunkData generateChunkData(World world, Random random, int chunkX, int chunkZ, BiomeGrid biome) {
+        SimplexOctaveGenerator generator = new SimplexOctaveGenerator(new Random(world.getSeed()), 8);
+        ChunkData chunk = createChunkData(world);
+        generator.setScale(0.005D);
+
+        for (int X = 0; X < 16; X++)
+            for (int Z = 0; Z < 16; Z++) {
+                currentHeight = (int) (generator.noise(chunkX*16+X, chunkZ*16+Z, 0.5D, 0.5D)*15D+50D);
+                chunk.setBlock(X, 50, Z, Material.GRASS);
+                chunk.setBlock(X, 49, Z, Material.OBSIDIAN);
+            }
+        return chunk;
+    }*/
+
     @Override
     public ChunkData generateChunkData(World world, Random random, int chunkX, int chunkZ, BiomeGrid biome) {
         SimplexOctaveGenerator generator = new SimplexOctaveGenerator(new Random(world.getSeed()), 8);
@@ -26,6 +41,9 @@ public class TycoonGenerator extends ChunkGenerator {
                 currentHeight = (int) (generator.noise(chunkX*16+X, chunkZ*16+Z, 0.5D, 0.5D)*15D+50D);
                 chunk.setBlock(X, 50, Z, Material.GRASS);
                 chunk.setBlock(X, 49, Z, Material.OBSIDIAN);
+                //for (int i = currentHeight-2; i > 0; i--)
+                //    chunk.setBlock(X, i, Z, Material.STONE);
+                //chunk.setBlock(X, 0, Z, Material.BEDROCK);
             }
         return chunk;
     }
